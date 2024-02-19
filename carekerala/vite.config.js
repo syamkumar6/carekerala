8fe807bd272cdea2b33b4d0c002832177b013238
Syam <135089954+syamkumar6@users.noreply.github.com>
2024-02-20 01:12:30 +0530

    Update vite.config.js

--- carekerala/vite.config.js
+++ carekerala/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Set the production mode
+    mode: 'production',
+    // Specify the output directory (adjust as needed)
+    outDir: 'dist',
+    // Enable minification for the production build
+    minify: 'terser',
+    // Enable splitting chunks to improve loading performance
+    chunkSizeWarningLimit: 600,
+  },
+});
