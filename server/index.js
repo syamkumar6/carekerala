6762991c9c11fadb6be42b3f15079a8c3eca735f
Syam <135089954+syamkumar6@users.noreply.github.com>
2024-02-20 10:40:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.use(cors({
   methods: ["POST", "GET", "DELETE",],
   credentials: true
 }))
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(upload.single('image'));
 
 
